adaf6adee9ec73bb026300f01aea3ca5635e29a6
Prasiddha Shrestha <prasiddhastha7@gmail.com>
2023-01-05 18:15:50 +0545

    Add project files.

---
+++ RandomClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CsharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            const int passwordLength = 10;
+            var buffer = new char[passwordLength];
+            
+            for (var i = 0; i < passwordLength; i++)
+            {
+
+                buffer[i]=(char)('a'+random.Next(0, 26));//generating random character
+            }
+            var password =new string(buffer);
+            Console.WriteLine(password);
+        }
+    }
+}
